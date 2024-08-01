afb1a4e829c02018c647ef9e1c57b57474a09041
chr91 <ch91fr@gmail.com>
2024-08-01 19:58:11 +0200

    custom keymap updated

--- keyboards/gmmk/pro/rev1/iso/keymaps/chr_custom/keymap.c
+++ keyboards/gmmk/pro/rev1/iso/keymaps/chr_custom/keymap.c
@@ -1,64 +1,39 @@
-/* Copyright 2021 Glorious, LLC <salman@pcgamingrace.com>
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
 #include QMK_KEYBOARD_H
 
-// clang-format off
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+void keyboard_post_init_user(void) {  
+	keymap_config.nkro = 1; // Enable NKRO
 
-//      ESC      F1       F2       F3       F4       F5       F6       F7       F8       F9       F10      F11      F12	     Prt           Rotary(Mute)
-//      ~        1        2        3        4        5        6        7        8        9        0         -       (=)	     BackSpc           Del
-//      Tab      Q        W        E        R        T        Y        U        I        O        P        [        ]                          PgUp
-//      Caps     A        S        D        F        G        H        J        K        L        ;        "        #        Enter             PgDn
-//      Sh_L     /        Z        X        C        V        B        N        M        ,        .        ?                 Sh_R     Up       End
-//      Ct_L     Win_L    Alt_L                               SPACE                               Alt_R    FN       Ct_R     Left     Down     Right
+	// Set default RGB
+    rgb_matrix_sethsv_noeeprom(191, 255, 255); // Violet
+    rgb_matrix_mode_noeeprom(RGB_MATRIX_SOLID_COLOR); // Set to a solid color mode
+}
 
-
-    // The FN key by default maps to a momentary toggle to layer 1 to provide access to the QK_BOOT key (to put the board into bootloader mode). Without
-    // this mapping, you have to open the case to hit the button on the bottom of the PCB (near the USB cable attachment) while plugging in the USB
-    // cable to get the board into bootloader mode - definitely not fun when you're working on your QMK builds. Remove this and put it back to KC_RGUI
-    // if that's your preference.
-    //
-    // To put the keyboard in bootloader mode, use FN+backspace. If you accidentally put it into bootloader, you can just unplug the USB cable and
-    // it'll be back to normal when you plug it back in.
-    //
-    // This keyboard defaults to 6KRO instead of NKRO for compatibility reasons (some KVMs and BIOSes are incompatible with NKRO).
-    // Since this is, among other things, a "gaming" keyboard, a key combination to enable NKRO on the fly is provided for convenience.
-    // Press Fn+N to toggle between 6KRO and NKRO. This setting is persisted to the EEPROM and thus persists between restarts.
-    [0] = LAYOUT(
-        KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PSCR,          KC_MUTE,
-        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,          KC_DEL,
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,                   KC_PGUP,
-        KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_NUHS, KC_ENT,           KC_PGDN,
-        KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT, KC_UP,   KC_END,
-        KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, MO(1),   KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
-    ),
-
-    [1] = LAYOUT(
-        _______, KC_MYCM, KC_WHOM, KC_CALC, KC_MSEL, KC_MPRV, KC_MNXT, KC_MPLY, KC_MSTP, KC_MUTE, KC_VOLD, KC_VOLU, _______, _______,          _______,
-        _______, RGB_TOG, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, QK_BOOT,          _______,
-        _______, _______, RGB_VAI, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                   _______,
-        _______, _______, RGB_VAD, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
-        _______, _______, _______, RGB_HUI, _______, _______, _______, NK_TOGG, _______, _______, _______, _______,          _______, RGB_MOD, _______,
-        _______, _______, _______,                            _______,                            _______, _______, _______, RGB_SPD, RGB_RMOD, RGB_SPI
-    ),
-
-
+enum custom_keycodes {
+    MACRO_PARENTHESIS = SAFE_RANGE,  // "()"
+    MACRO_CURLY_BRACES,              // "{}"
+    MACRO_SQUARE_BRACKETS,           // "[]"
+    MACRO_ANGLE_BRACKETS,            // "<>"
+	MACRO_TOGGLE_SNAP_TAP
 };
-// clang-format on
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+	[0] = LAYOUT(
+		KC_ESC, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_DEL, KC_MUTE,
+		KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC, KC_INS,
+		KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_PGUP,
+		MO(1), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_NUHS, KC_ENT, KC_PGDN,
+		KC_LSFT, KC_NUBS, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP, KC_CAPS,
+		KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, KC_RALT, KC_LGUI, MACRO_TOGGLE_SNAP_TAP, KC_LEFT, KC_DOWN, KC_RGHT
+	),
+	[1] = LAYOUT(
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PSCR,
+		KC_TRNS, KC_ENT, KC_UP, KC_BSPC, KC_DEL, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_NO, KC_LEFT, KC_DOWN, KC_RGHT, KC_F, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS, MACRO_PARENTHESIS, MACRO_CURLY_BRACES, MACRO_SQUARE_BRACKETS, MACRO_ANGLE_BRACKETS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, QK_BOOT,
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_LCTL, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+	)
+};
 
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
@@ -66,3 +41,96 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
     [1] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS) }
 };
 #endif
+
+static bool is_snap_tap_enabled = false;
+static bool is_a_active = false;
+static bool is_d_active = false;
+static bool a_held = false;
+static bool d_held = false;
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+		case KC_A:
+			if (!is_snap_tap_enabled) {
+				return true;
+			}
+		
+			if (record->event.pressed) {
+				if (is_d_active) {
+					unregister_code(KC_D);
+					is_d_active = false;
+				}
+				
+				is_a_active = true;
+			} else {
+				is_a_active = false;
+			}
+			
+			return true;
+			
+		case KC_D:
+			if (!is_snap_tap_enabled) {
+				return true;
+			}
+		
+			if (record->event.pressed) {
+				if (is_a_active) {
+					unregister_code(KC_A);
+					is_a_active = false;
+				}
+				
+				is_d_active = true;
+			} else {
+				is_d_active = false;
+			}
+			
+			return true;
+			
+		case MACRO_TOGGLE_SNAP_TAP:
+			if (record->event.pressed) {
+				if (is_snap_tap_enabled) {					
+					is_snap_tap_enabled = false;
+					send_string("Snap Tap deactivated");
+				} else {
+					is_snap_tap_enabled = true;
+					send_string("Snap Tap activated");
+				}
+			}
+			
+			return false;
+		
+        case MACRO_PARENTHESIS:
+            if (record->event.pressed) {
+                tap_code16(S(KC_8));       // Shift + 8
+                tap_code16(S(KC_9));       // Shift + 9
+            }
+			
+            return false;
+
+        case MACRO_CURLY_BRACES:
+            if (record->event.pressed) {
+                tap_code16(RALT(KC_7));  // AltGr + 7
+                tap_code16(RALT(KC_0));  // AltGr + 0
+            }
+			
+            return false;
+
+        case MACRO_SQUARE_BRACKETS:
+            if (record->event.pressed) {
+                tap_code16(RALT(KC_8));  // AltGr + 8
+                tap_code16(RALT(KC_9));  // AltGr + 9
+            }
+			
+            return false;
+
+        case MACRO_ANGLE_BRACKETS:
+            if (record->event.pressed) {
+                tap_code16(KC_NUBS);        // Press "<"
+                tap_code16(S(KC_NUBS));   // Shift + "<"
+            }
+			
+            return false;
+    }
+	
+    return true; // Process all other keycodes normally
+};
